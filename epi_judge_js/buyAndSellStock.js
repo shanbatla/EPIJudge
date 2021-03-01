718a39a57fc146faea397a5daa71d6ee8d82aa0b
Shan Batla <shan.batla@gmail.com>
2021-02-28 19:41:00 -0500

    Add another implementation of buy_and_sell_stock in js.

--- epi_judge_js/buyAndSellStock.js
+++ epi_judge_js/buyAndSellStock.js
@@ -13,7 +13,25 @@ function buyAndSellStock(prices) {
     return maxProfit;
 }
 
+function buyAndSellStockv2(prices) {
+    let minPriceSoFar;
+    let maxProfit = 0;
+
+    for (let idx = 0; idx < prices.length; idx++) {
+        if (idx === 0) {
+            minPriceSoFar = prices[idx];
+            continue;
+        } else {
+            minPriceSoFar = Math.min(minPriceSoFar, prices[idx]);
+            let profitToday = prices[idx] - minPriceSoFar;
+            maxProfit = Math.max(maxProfit, profitToday); 
+        }
+    }
+
+    return maxProfit;
+}
+
 const prices = [310, 315, 275,295, 260, 270, 290, 230, 255, 250];
 const ans = 30;
 
-assert.strictEqual(buyAndSellStock(prices), ans);
+assert.strictEqual(buyAndSellStockv2(prices), ans);
