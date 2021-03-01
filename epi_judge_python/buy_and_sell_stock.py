5b01e4925bee7fd56fa52baf7bf42fd404f0ebc8
Shan Batla <shan.batla@gmail.com>
2021-02-28 19:02:49 -0500

    Implement buy_and_sell_stocks in py.

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,9 +4,15 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    min_price_so_far = float('inf')
+    max_profit = 0
 
+    for price in prices:
+        max_profit_today = price - min_price_so_far
+        max_profit = max(max_profit, max_profit_today)
+        min_price_so_far = min(price, min_price_so_far)
+
+    return max_profit
 
 if __name__ == '__main__':
     exit(
